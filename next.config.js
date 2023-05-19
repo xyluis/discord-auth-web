613781e76c6083f4076cd3df7593847d4ed68a9f
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2023-05-19 13:53:54 -0400

    feat: add full application

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['discordapp.com', 'cdn.discordapp.com'],
+  },
+}
 
 module.exports = nextConfig
